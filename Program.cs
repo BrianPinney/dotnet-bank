d4252dcc250f6d8dc92bb0f63e87b6d53d2e987c
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-09 14:42:18 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+
+// Console.WriteLine("Hello, World!");
+int age = 39;
+string name = "Brian";
+bool isHere = true;
+double salary = 220000.01;
+Console.WriteLine($"My name is {name}, I'm {age} and am here. {isHere}");
+Console.WriteLine($"I make ${salary} a year");
